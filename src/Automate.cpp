8eaf1f08835958ae78ade26d729d91b0c4d602e5
Bastien-Viatge <bastien.viatge@insa-lyon.fr>
2015-03-10 11:32:50 +0100

    mise en place de l'enum

--- src/Automate.cpp
+++ src/Automate.cpp
@@ -1,6 +1,8 @@
 #include "Automate.h"
+#include "Etat.h"
 
 //Method that allows a shift of the symbol and that stocks the state and symbol into the stack
 void Automate::shift(Symbol s, Etat *e){
-
+	symbolStack.push(s);
+	stateStack.push(e);
 }
--- src/Automate.h
+++ src/Automate.h
@@ -2,8 +2,10 @@
 #define AUTOMATE_H
 
 #include "Symbol.h"
+#include <stack>
 
 class Etat;
+using namespace std; 
 
 class Automate{
 public:
@@ -12,6 +14,9 @@ public:
 	void shift(Symbol s, Etat *e);
 	//If we don't use a pointer, it has no choice but to be an object of the class
 	//Etat, and Etat is abstract.
+private:
+	stack<Symbol> symbolStack;
+	stack<Etat*> stateStack;
 };
 
 #endif
