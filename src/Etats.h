9b1952f0fc5c1050835d2b04624c4579333c8c35
Bastien-Viatge <bastien.viatge@insa-lyon.fr>
2015-03-10 13:23:24 +0100

    gloire au glorieux Vadim et aux regex

--- src/Etats.h
+++ src/Etats.h
@@ -7,7 +7,7 @@
 
 class E0:public Etat {
 	public:
-		 E0(string name) : Etat(name){}
+		 E0() : Etat("E0"){}
 		~E0() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
@@ -15,7 +15,7 @@ class E0:public Etat {
 
 class E1:public Etat {
 	public:
-		 E1(string name) : Etat(name){}
+		 E1() : Etat("E1"){}
 		~E1() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
@@ -23,7 +23,7 @@ class E1:public Etat {
 
 class E2:public Etat {
 	public:
-		 E2(string name) : Etat(name){}
+		 E2() : Etat("E2"){}
 		~E2() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
@@ -31,7 +31,7 @@ class E2:public Etat {
 
 class E3:public Etat {
 	public:
-		 E3(string name) : Etat(name){}
+		 E3() : Etat("E3"){}
 		~E3() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
@@ -39,7 +39,7 @@ class E3:public Etat {
 
 class E4:public Etat {
 	public:
-		 E4(string name) : Etat(name){}
+		 E4() : Etat("E4"){}
 		~E4() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
@@ -47,259 +47,259 @@ class E4:public Etat {
 
 class E5:public Etat {
 	public:
-		 E5(string name) : Etat(name){}
+		 E5() : Etat("E5"){}
 		~E5() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E6:public Etat {
 	public:
-		 E6(string name) : Etat(name){}
+		 E6() : Etat("E6"){}
 		~E6() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E7:public Etat {
 	public:
-		 E7(string name) : Etat(name){}
+		 E7() : Etat("E7"){}
 		~E7() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E8:public Etat {
 	public:
-		 E8(string name) : Etat(name){}
+		 E8() : Etat("E8"){}
 		~E8() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E9:public Etat {
 	public:
-		 E9(string name) : Etat(name){}
+		 E9() : Etat("E9"){}
 		~E9() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E10:public Etat {
 	public:
-		 E10(string name) : Etat(name){}
+		 E10() : Etat("E10"){}
 		~E10() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E11:public Etat {
 	public:
-		 E11(string name) : Etat(name){}
+		 E11() : Etat("E11"){}
 		~E11() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E12:public Etat {
 	public:
-		 E12(string name) : Etat(name){}
+		 E12() : Etat("E12"){}
 		~E12() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E13:public Etat {
 	public:
-		 E13(string name) : Etat(name){}
+		 E13() : Etat("E13"){}
 		~E13() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E14:public Etat {
 	public:
-		 E14(string name) : Etat(name){}
+		 E14() : Etat("E14"){}
 		~E14() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E15:public Etat {
 	public:
-		 E15(string name) : Etat(name){}
+		 E15() : Etat("E15"){}
 		~E15() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E16:public Etat {
 	public:
-		 E16(string name) : Etat(name){}
+		 E16() : Etat("E16"){}
 		~E16() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E17:public Etat {
 	public:
-		 E17(string name) : Etat(name){}
+		 E17() : Etat("E17"){}
 		~E17() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E18:public Etat {
 	public:
-		 E18(string name) : Etat(name){}
+		 E18() : Etat("E18"){}
 		~E18() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E19:public Etat {
 	public:
-		 E19(string name) : Etat(name){}
+		 E19() : Etat("E19"){}
 		~E19() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E20:public Etat {
 	public:
-		 E20(string name) : Etat(name){}
+		 E20() : Etat("E20"){}
 		~E20() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E21:public Etat {
 	public:
-		 E21(string name) : Etat(name){}
+		 E21() : Etat("E21"){}
 		~E21() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E22:public Etat {
 	public:
-		 E22(string name) : Etat(name){}
+		 E22() : Etat("E22"){}
 		~E22() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E23:public Etat {
 	public:
-		 E23(string name) : Etat(name){}
+		 E23() : Etat("E23"){}
 		~E23() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E24:public Etat {
 	public:
-		 E24(string name) : Etat(name){}
+		 E24() : Etat("E24"){}
 		~E24() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E25:public Etat {
 	public:
-		 E25(string name) : Etat(name){}
+		 E25() : Etat("E25"){}
 		~E25() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E26:public Etat {
 	public:
-		 E26(string name) : Etat(name){}
+		 E26() : Etat("E26"){}
 		~E26() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E27:public Etat {
 	public:
-		 E27(string name) : Etat(name){}
+		 E27() : Etat("E27"){}
 		~E27() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E28:public Etat {
 	public:
-		 E28(string name) : Etat(name){}
+		 E28() : Etat("E28"){}
 		~E28() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E29:public Etat {
 	public:
-		 E29(string name) : Etat(name){}
+		 E29() : Etat("E29"){}
 		~E29() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E30:public Etat {
 	public:
-		 E30(string name) : Etat(name){}
+		 E30() : Etat("E30"){}
 		~E30() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E31:public Etat {
 	public:
-		 E31(string name) : Etat(name){}
+		 E31() : Etat("E31"){}
 		~E31() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E32:public Etat {
 	public:
-		 E32(string name) : Etat(name){}
+		 E32() : Etat("E32"){}
 		~E32() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E33:public Etat {
 	public:
-		 E33(string name) : Etat(name){}
+		 E33() : Etat("E33"){}
 		~E33() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E34:public Etat {
 	public:
-		 E34(string name) : Etat(name){}
+		 E34() : Etat("E34"){}
 		~E34() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E35:public Etat {
 	public:
-		 E35(string name) : Etat(name){}
+		 E35() : Etat("E35"){}
 		~E35() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E36:public Etat {
 	public:
-		 E36(string name) : Etat(name){}
+		 E36() : Etat("E36"){}
 		~E36() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E37:public Etat {
 	public:
-		 E37(string name) : Etat(name){}
+		 E37() : Etat("E37"){}
 		~E37() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E38:public Etat {
 	public:
-		 E38(string name) : Etat(name){}
+		 E38() : Etat("E38"){}
 		~E38() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E39:public Etat {
 	public:
-		 E39(string name) : Etat(name){}
+		 E39() : Etat("E39"){}
 		~E39() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E40:public Etat {
 	public:
-		 E40(string name) : Etat(name){}
+		 E40() : Etat("E40"){}
 		~E40() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E41:public Etat {
 	public:
-		 E41(string name) : Etat(name){}
+		 E41() : Etat("E41"){}
 		~E41() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
