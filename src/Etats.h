cd501c33296a327b49f6a2c7df6b8060f7df7cc7
Bastien-Viatge <bastien.viatge@insa-lyon.fr>
2015-03-10 12:01:30 +0100

    ajout de Symbols.h, ps encore tester (pas taper pas taper !)

--- src/Etats.h
+++ src/Etats.h
@@ -5,7 +5,7 @@
 #include "Etat.h"
 
 class E0:public Etat {
-	public :
+	public:
 		 E0(string name) : Etat(name){}
 		~E0() {}
   private:
@@ -13,7 +13,7 @@ class E0:public Etat {
 };
 
 class E1:public Etat {
-	public :
+	public:
 		 E1(string name) : Etat(name){}
 		~E1() {}
   private:
@@ -21,7 +21,7 @@ class E1:public Etat {
 };
 
 class E2:public Etat {
-	public :
+	public:
 		 E2(string name) : Etat(name){}
 		~E2() {}
   private:
@@ -29,273 +29,273 @@ class E2:public Etat {
 };
 
 class E3:public Etat {
-	public :
+	public:
 		 E3(string name) : Etat(name){}
 		~E3() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E4:public Etat {
-	public :
+	public:
 		 E4(string name) : Etat(name){}
 		~E4() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E5:public Etat {
-	public :
+	public:
 		 E5(string name) : Etat(name){}
 		~E5() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E6:public Etat {
-	public :
+	public:
 		 E6(string name) : Etat(name){}
 		~E6() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E7:public Etat {
-	public :
+	public:
 		 E7(string name) : Etat(name){}
 		~E7() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E8:public Etat {
-	public :
+	public:
 		 E8(string name) : Etat(name){}
 		~E8() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E9:public Etat {
-	public :
+	public:
 		 E9(string name) : Etat(name){}
 		~E9() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E10:public Etat {
-	public :
+	public:
 		 E10(string name) : Etat(name){}
 		~E10() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E11:public Etat {
-	public :
+	public:
 		 E11(string name) : Etat(name){}
 		~E11() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E12:public Etat {
-	public :
+	public:
 		 E12(string name) : Etat(name){}
 		~E12() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E13:public Etat {
-	public :
+	public:
 		 E13(string name) : Etat(name){}
 		~E13() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E14:public Etat {
-	public :
+	public:
 		 E14(string name) : Etat(name){}
 		~E14() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E15:public Etat {
-	public :
+	public:
 		 E15(string name) : Etat(name){}
 		~E15() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E16:public Etat {
-	public :
+	public:
 		 E16(string name) : Etat(name){}
 		~E16() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E17:public Etat {
-	public :
+	public:
 		 E17(string name) : Etat(name){}
 		~E17() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E18:public Etat {
-	public :
+	public:
 		 E18(string name) : Etat(name){}
 		~E18() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E19:public Etat {
-	public :
+	public:
 		 E19(string name) : Etat(name){}
 		~E19() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E20:public Etat {
-	public :
+	public:
 		 E20(string name) : Etat(name){}
 		~E20() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E21:public Etat {
-	public :
+	public:
 		 E21(string name) : Etat(name){}
 		~E21() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E22:public Etat {
-	public :
+	public:
 		 E22(string name) : Etat(name){}
 		~E22() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E23:public Etat {
-	public :
+	public:
 		 E23(string name) : Etat(name){}
 		~E23() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E24:public Etat {
-	public :
+	public:
 		 E24(string name) : Etat(name){}
 		~E24() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E25:public Etat {
-	public :
+	public:
 		 E25(string name) : Etat(name){}
 		~E25() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E26:public Etat {
-	public :
+	public:
 		 E26(string name) : Etat(name){}
 		~E26() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E27:public Etat {
-	public :
+	public:
 		 E27(string name) : Etat(name){}
 		~E27() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E28:public Etat {
-	public :
+	public:
 		 E28(string name) : Etat(name){}
 		~E28() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E29:public Etat {
-	public :
+	public:
 		 E29(string name) : Etat(name){}
 		~E29() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E30:public Etat {
-	public :
+	public:
 		 E30(string name) : Etat(name){}
 		~E30() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E31:public Etat {
-	public :
+	public:
 		 E31(string name) : Etat(name){}
 		~E31() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E32:public Etat {
-	public :
+	public:
 		 E32(string name) : Etat(name){}
 		~E32() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E33:public Etat {
-	public :
+	public:
 		 E33(string name) : Etat(name){}
 		~E33() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E34:public Etat {
-	public :
+	public:
 		 E34(string name) : Etat(name){}
 		~E34() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E35:public Etat {
-	public :
+	public:
 		 E35(string name) : Etat(name){}
 		~E35() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E36:public Etat {
-	public :
+	public:
 		 E36(string name) : Etat(name){}
 		~E36() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E37:public Etat {
-	public :
+	public:
 		 E37(string name) : Etat(name){}
 		~E37() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E38:public Etat {
-	public :
+	public:
 		 E38(string name) : Etat(name){}
 		~E38() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E39:public Etat {
-	public :
+	public:
 		 E39(string name) : Etat(name){}
 		~E39() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E40:public Etat {
-	public :
+	public:
 		 E40(string name) : Etat(name){}
 		~E40() {}
   private:
 		 bool transition (Automate &automate, Symbol *s);
 };
 class E41:public Etat {
-	public :
+	public:
 		 E41(string name) : Etat(name){}
 		~E41() {}
   private:
