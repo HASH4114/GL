bff8b30f1107d921469a83a11b0079c7d3aa04a3
root <root@localhost.localdomain>
2015-03-10 11:49:48 +0100

    cmd line

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "Etat.h"
 #include "Expr.h"
 
-
+#include "boost/program_options.hpp"
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello, world!" << endl;
-    return 0;
+    namespace po = boost::program_options;
+    po::options_description desc("Options");
+    desc.add_options() ("help,", "Print help messages") 
+                    ("print,p", "Affichage du programme")
+                    ("analyse,a", "Analyse statique du programme")
+                    ("execution,e", "Execution du programme")
+                    ("transformation,o", "Transformation du programme pour simplifier")
+                    ("file,f", po::value<std::string>()->required(), "Source file") 
+    ;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);    
+    if (vm.count("help")) 
+    {
+        cout << desc << "\n";
+        return 1;
+    }
+    if (vm.count("file")) 
+    {
+        std::ifstream ifs(vm["file"].as<std::string>().c_str());
+        std::string content( (std::istreambuf_iterator<char>(ifs) ), (std::istreambuf_iterator<char>()) );
+        // pour mastho et felix
+        //appel au parseur avec content
+    }
+
+
+   return 0;
 }
